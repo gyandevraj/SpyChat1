12b8a6356abf2f72704f48f4200c3419e045c3c1
gyandevraj <37468445+gyandevraj@users.noreply.github.com>
2018-03-23 00:22:26 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print 'Let\'s get started with our project SpyChat.'
+print "Hello User."
+My_Name = raw_input("My Name Is.:-")
+print "My Name Is " + My_Name
+Spy_Name = raw_input("What's Your Name? ")
+print "Welcome " + Spy_Name + " Glad to see you."
+Spy_Salutation = raw_input("What should we call you (Mr. or Ms.)?")
+print Spy_Salutation + " " + Spy_Name
+Spy_Name = Spy_Salutation + " " + Spy_Name
+print "Alright " + Spy_Name + " " + "We Would Like To Know A Little Bit More About You..."
+Spy_Code_Name = raw_input("Please Enter Your Code Name ")
+print "Welcome " + Spy_Code_Name + " " + "You Can Place Your Order Here."
