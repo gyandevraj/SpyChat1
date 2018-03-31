06faf53f54d06c51a00310bc6ff4e76156a46c19
gyandevraj <37468445+gyandevraj@users.noreply.github.com>
2018-03-31 21:08:33 +0530

    status uploaded and friend added

--- main.py
+++ main.py
@@ -2,17 +2,70 @@ from Existing_Spy import Spy_Name, Spy_Code_Name, Spy_Age
 
 print "Hello!!! \nWelcome To SpyChat."
 
+STATUS_MESSAGE = ["gOOD mORNInG", "HeLLo", "NAmaStE"]
+
+friends_name = []
+friends_age = []
+friends_Code_Name = []
+friend_is_online = []
+
+
+def add_status(c_status):
+    if c_status != None:
+        print "your current status is " + c_status
+    else:
+        print "you don't have any status currently"
+    existing_status = raw_input("Do you want to add new status? Y/N ")
+    if existing_status.upper() == "Y":
+        new_status = raw_input("Enter your status: ")
+        if len(new_status) > 0:
+            STATUS_MESSAGE.append(new_status)
+
+    elif existing_status.upper() == "N":
+        serial_no = 1
+        for old_status in STATUS_MESSAGE:
+            print str(serial_no) + ". " + old_status
+            serial_no = serial_no + 1
+        user_choice = input("Enter your choice: ")
+        new_status = STATUS_MESSAGE[user_choice - 1]
+    update_status = new_status
+    return update_status
+
+
+def add_friend():
+    frnd_name = raw_input("What is your name? ")
+    frnd_age = input("What is your age? ")
+    frnd_code_name = raw_input("What is your code name? ")
+    if len(frnd_name) > 3 and 18 < frnd_age < 50 and frnd_code_name > 3:
+        friends_name.append(frnd_name)
+        friends_age.append(frnd_age)
+        friends_Code_Name.append(frnd_code_name)
+        friend_is_online.append(True)
+    else:
+        print "Friend's Can't be added"
+    return len(friends_name)
+
+
 def Start_Chat(Spy_Code_Name, Spy_Age):
     print Spy_Code_Name + " What Do You Want To Do?"
+
+    current_status = None
+
     Show_Menu = True
     while Show_Menu:
-        Choices = input ("1. Update Status \n2. Add A Friend \n3. Send A Message \n0. Exit ")
+        Choices = input("1. Update Status \n2. Add A Friend \n3. Send A Message \n0. Exit ")
         if Choices == 1:
-            Status = raw_input("Enter A Status ")
-            print "Your Status Is. " + Status
+
+            current_status = add_status(current_status)
+            print "updated status " + current_status
+
         elif Choices == 2:
-            Add_Friend = raw_input("Enter Friend Name To Add ")
-            print "Your Friend " + Add_Friend + " Is Added."
+
+            no_of_friends = add_friend()
+            print "you have " + str(no_of_friends) + " friends"
+            print "your friend list is: \n" \
+                  "" +str(friends_name)
+
         elif Choices == 3:
             Message = raw_input("Type A Message To Send. ")
             print "Your Message \n" + Message
@@ -22,11 +75,12 @@ def Start_Chat(Spy_Code_Name, Spy_Age):
         else:
             print "Choose A Correct Option."
 
+
 Existing_Spy = raw_input("Are You A New User (Y/N) ")
 
 if Existing_Spy == "N".lower() or Existing_Spy == "n".upper():
     print "Welcome Back Agent " + Spy_Name + ". \nCode Name " + Spy_Code_Name + "\nAge " + str(Spy_Age)
-    Start_Chat(Spy_Code_Name,Spy_Age)
+    Start_Chat(Spy_Code_Name, Spy_Age)
 
 
 elif Existing_Spy == "Y".lower() or Existing_Spy == "y".upper():
@@ -56,8 +110,8 @@ elif Existing_Spy == "Y".lower() or Existing_Spy == "y".upper():
 
                         print "Authentication Completed. \nNow You Can Start Your Chat."
 
-                        Spy_Work_Field = input("Select Your Working Field." 
-                                                    "\n1. Under Cover \n2. Analysis \n3. Technician ")
+                        Spy_Work_Field = input("Select Your Working Field."
+                                               "\n1. Under Cover \n2. Analysis \n3. Technician ")
                         if Spy_Work_Field == 1:
                             print "Welcome Agent " + Spy_Code_Name + "\n" + "You Can Leave Your Message Now."
                             Start_Chat(Spy_Code_Name, Spy_Age)
